6b53c36cf157a5a6c5062b5dd7c9600a7fbb100f
Yen On Chan <yenon118@gmail.com>
2023-08-28 23:42:55 -0500

    Changed the behavior of open the user manual

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 
 function downloadUserManual() {
 	let downloadAnchorNode = document.createElement('a');
-	downloadAnchorNode.setAttribute("href", "php/downloadUserManual.php?File=GenVarX_User_Manual.pdf");
+	// downloadAnchorNode.setAttribute("href", "php/downloadUserManual.php?File=GenVarX_User_Manual.pdf");
+	downloadAnchorNode.setAttribute("href", "assets/User_Manual/GenVarX_User_Manual.pdf");
+	downloadAnchorNode.setAttribute("target", "_blank");
 	document.body.appendChild(downloadAnchorNode); // required for firefox
 	downloadAnchorNode.click();
 	downloadAnchorNode.remove();
